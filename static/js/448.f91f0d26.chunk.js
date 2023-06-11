7c30fa316a35dafdfcbcfd409a6ac501aa1471bc
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-06-11 07:02:09 +0000

    Deploying to gh-pages from @ Alex-XAG/goit-react-hw-05-movies@2d9c644e4c394b6f08191b1658ef21a00b3a3014 🚀

---
+++ static/js/448.f91f0d26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[448],{448:function(e,r,n){n.r(r),n.d(r,{Subbreeds:function(){return u}});var s=n(689),t=n(184),u=function(){var e=(0,s.UO)().dogId;return(0,t.jsxs)("div",{children:["Subbreeds: ",e]})}}}]);
+//# sourceMappingURL=448.f91f0d26.chunk.js.map
