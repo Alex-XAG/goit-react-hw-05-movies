15f7d7368ca3aee37f271547295cc4f9f357f0ab
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-06-15 11:02:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { App } from 'components/App';
-import { AppStudy } from 'components/AppStudy/AppStudy';
+import { App } from 'components/App';
+// import { AppStudy } from 'components/AppStudy/AppStudy';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies">
-      {/* <App /> */}
-      <AppStudy />
+      <App />
+      {/* <AppStudy /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
